58981efa668369ce6734d878b01feec85e4b2f49
Aayush Kumar Sahu <aayushmau5@gmail.com>
2026-01-22 10:50:40 +0530

    fix not_found page

--- lib/event_horizon_web/live/not_found_live.ex
+++ lib/event_horizon_web/live/not_found_live.ex
@@ -7,13 +7,13 @@ defmodule EventHorizonWeb.NotFoundLive do
 
   def render(assigns) do
     ~H"""
-    <Layouts.app current_path={@current_path}>
+    <Layouts.app current_path={@current_path} socket={@socket}>
       <div class="text-center mb-20 animate-fadeIn">
         <div class="w-[60%] h-auto mx-auto my-4">
           <img src="/images/cat.jpg" alt="Cat image" class="rounded-lg" />
         </div>
         <h1 class="font-[Handwriting] text-4xl font-bold">Oh no, you found the cat!</h1>
-        <.link href="/" class="styledLink mx-auto mt-4">
+        <.link navigate="/" class="styledLink mx-auto mt-4">
           <.icon name="hero-arrow-left" class="w-6 h-6 mr-2" /> Leave the cat be
         </.link>
       </div>
