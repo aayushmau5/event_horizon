49901f8f0e644ce175bc746f0b3fb55579e08296
Aayush Kumar Sahu <aayushmau5@gmail.com>
2026-01-25 21:08:02 +0530

    streamline mix post.new task

--- lib/mix/tasks/post.new.ex
+++ lib/mix/tasks/post.new.ex
@@ -1,15 +1,42 @@
 defmodule Mix.Tasks.Post.New do
-  @moduledoc "Create a new post. Run mix post.new \"my-new-post\""
+  @moduledoc """
+  Create a new post.
+
+  Usage: mix post.new "Post Title" --slug my-post-slug [--description "..."] [--dynamic]
+
+  ## Options
+
+    * `--slug` - (required) the slug for the post filename
+    * `--description` - (optional) post description, can also be passed as second positional arg
+    * `--dynamic` - (optional) mark the post as dynamic
+
+  ## Examples
+
+      mix post.new "My New Post" --slug my-new-post
+      mix post.new "My New Post" "A description" --slug my-new-post --dynamic
+      mix post.new "My New Post" --slug my-new-post --description "A description"
+  """
   use Mix.Task
+
+  @switches [slug: :string, description: :string, dynamic: :boolean]
 
   @impl true
   def run(args) do
+    {opts, positional, _invalid} = OptionParser.parse(args, strict: @switches)
+
+    title = Enum.at(positional, 0)
+    slug = Keyword.get(opts, :slug)
+
+    unless title && slug do
+      Mix.raise("Missing required arguments. Usage: mix post.new \"Title\" --slug my-slug")
+    end
+
+    description = Keyword.get(opts, :description) || Enum.at(positional, 1) || ""
+    dynamic? = Keyword.get(opts, :dynamic, false)
+
     path = Application.app_dir(:event_horizon, "priv/posts/")
-    title = Enum.at(args, 0) || "new-post"
-    description = Enum.at(args, 1) || "description"
-    dynamic? = Enum.at(args, 2) === "--dynamic" || false
-    File.write(Path.join(path, "#{title}.md"), init_blog_content(title, description, dynamic?))
-    Mix.shell().info("Done!")
+    File.write(Path.join(path, "#{slug}.md"), init_blog_content(title, description, dynamic?))
+    Mix.shell().info("Created post: #{slug}.md")
   end
 
   defp init_blog_content(title, description, dynamic?) do
