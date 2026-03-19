2e8c926ec6e2d03ffc38c6ed600cc61e2c691a07
Aayush Kumar Sahu <aayushmau5@gmail.com>
2026-03-19 13:01:20 +0530

    images

--- lib/event_horizon_web/live/about_live/index.ex
+++ lib/event_horizon_web/live/about_live/index.ex
@@ -1,8 +1,30 @@
 defmodule EventHorizonWeb.AboutLive.Index do
   use EventHorizonWeb, :live_view
 
+  @megu_stamps [
+    %{
+      id: 0,
+      src: "/images/megu.jpeg"
+    },
+    %{
+      id: 1,
+      src: "/images/cat.jpg"
+    },
+    %{
+      id: 2,
+      src: "/images/megu-space.webp"
+    },
+    %{
+      id: 3,
+      src: "/images/megu-banana.webp"
+    }
+  ]
+
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, page_title: "About | Aayush Sahu")}
+    {:ok,
+     socket
+     |> assign(page_title: "About | Aayush Sahu")
+     |> stream(:megu_stamps, @megu_stamps)}
   end
 end
